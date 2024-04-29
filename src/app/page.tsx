de18f1e0c826efa77bb0ae5b9b9b870c40de8dec
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-29 22:20:05 +0530

    added Next Image and taint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { getMyImages } from "~/server/queries";
+import Image from "next/image";
 
 export const dynamic = "force-dynamic";
 
@@ -8,10 +9,16 @@ async function Images() {
     const images = await getMyImages();
 
     return ( 
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap justify-center gap-4">
             {images.map((image) => (
-                <div key={image.id} className="w-48 flex flex-col">
-                    <img src={image.url} alt="image"/>
+                <div key={image.id} className="w-48 h-48 flex flex-col">
+                    <Image 
+                        src={image.url} 
+                        style={{objectFit:"contain"}} 
+                        width={192}
+                        height={192}
+                        alt={image.name}
+                    />
                     <div>{image.name}</div>
                 </div>
             ))}
