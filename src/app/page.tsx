c1bee48d36f2ffcf6adeb98360bf6a09c4db3f9d
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-29 11:58:47 +0530

    setting up databse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,33 @@ import { db } from "../server/db/index";
 
 export const dynamic = "force-dynamic";
 
-const mockURL = [
-    "https://utfs.io/f/2789cddd-c039-4d13-9872-f97ecfe7689f-ja6c04.com_death-stranding-fanart-4k_3625x2160.jpg",
-    "https://utfs.io/f/2e6bf210-49e4-49bf-84c4-311747a041f5-3mczkm.jpg",
-    "https://utfs.io/f/c9ae0bf6-494b-4f59-ad18-2548bd07b81b-a1jsaj.png",
-    "https://utfs.io/f/bb6fab1b-c859-4a4a-b3a9-1a9fe190bc0b-v6mnai.png"
-]
+// const mockURL = [
+//     "https://utfs.io/f/2789cddd-c039-4d13-9872-f97ecfe7689f-ja6c04.com_death-stranding-fanart-4k_3625x2160.jpg",
+//     "https://utfs.io/f/2e6bf210-49e4-49bf-84c4-311747a041f5-3mczkm.jpg",
+//     "https://utfs.io/f/c9ae0bf6-494b-4f59-ad18-2548bd07b81b-a1jsaj.png",
+//     "https://utfs.io/f/bb6fab1b-c859-4a4a-b3a9-1a9fe190bc0b-v6mnai.png"
+// ]
 
-const mockImages = mockURL.map((url, index) => ({
-    id: index + 1,
-    url
-}));
+// const mockImages = mockURL.map((url, index) => ({
+//     id: index + 1,
+//     url
+// }));
 
 export default async function HomePage() {
 
-    const posts = await db.query.posts.findMany();
+    const images = await db.query.images.findMany({
+        orderBy: (model, {desc}) => desc(model.id),
+    });
 
-    console.log(posts);
+    console.log(images);
 
     return (
         <main className="">
             <div className="flex flex-wrap gap-4">
-                {posts.map((post) => (
-                    <div key={post.id}>{post.name}</div>
-                ))}
-                {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-                    <div key={image.id + "-" + index} className="w-48">
+                {[...images, ...images, ...images].map((image, index) => (
+                    <div key={image.id + "-" + index} className="w-48 flex flex-col">
                         <img src={image.url} alt="image"/>
+                        <div>{image.name}</div>
                     </div>
                 ))}
             </div>
