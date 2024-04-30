8c3a25dbe6a6f3baff3a141bed92b9cb85e85292
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-04-30 23:01:05 +0530

    removing spaces

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -5,9 +5,7 @@ import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
 
 export default function TopNav() {
-
     const router = useRouter();
-
     return (
         <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
             <div>Gallery</div>
