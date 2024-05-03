4bc521168d04ca7b967c81da7a06bb53fae236ce
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-05-03 13:21:40 +0530

    fixing opacity issue and padding in view page and full image display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,11 @@ import Link from "next/link";
 
 export const dynamic = "force-dynamic";
 
-
 async function Images() {
     const images = await getMyImages();
 
     return ( 
-        <div className="flex flex-wrap justify-center gap-4">
+        <div className="flex flex-wrap justify-center gap-4 p-4">
             {images.map((image) => (
                 <div key={image.id} className="w-48 h-48 flex flex-col">
                     <Link href={`/img/${image.id}`}>
