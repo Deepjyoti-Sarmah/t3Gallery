11ac8d7f0d8fef744d0a90a08704e6b0d3ab0d56
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2024-05-14 16:00:27 +0530

    displaying full screen image in signout view

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -9,13 +9,13 @@ export interface Image {
 }
 
 const Card = ({id, url, name}: Image) => {
-  return (
+    return (
         <div key={id} className="flex flex-col items-center justify-center border border-slate-800 bg-slate-950/90 shadow-lg rounded-lg p-2">
             <Link href={`/img/${id}`}>
                 <div className=" w-48 h-48 relative ">
-                    <Image 
-                        src={url} 
-                        style={{objectFit:"contain"}} 
+                    <Image
+                        src={url}
+                        style={{ objectFit: "contain" }}
                         fill
                         alt={name}
                         className="w-full h-full object-cover"
@@ -23,7 +23,7 @@ const Card = ({id, url, name}: Image) => {
                 </div>
             </Link>
             <div className="p-1 font-thin text-slate-400 text-center">
-                <span>{name.length > 15 ? (name).slice(0,15)+"..." : (name)}</span>
+                <span>{name.length > 15 ? (name).slice(0, 15) + "..." : (name)}</span>
             </div>
         </div>
     );
---
+++ src/common/full-page-image-logout.tsx
@@ -0,0 +1,47 @@
+
+import { clerkClient } from "@clerk/nextjs/server";
+import { mockImages } from "~/app/_components/mokeImages";
+import { Button } from "~/components/ui/button";
+import { deleteImage, getImage } from "~/server/queries";
+
+export default async function FullPageImageView(props: {id: number}) {
+    const idAsNumber = Number(props.id);
+    if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+    const image = await getImage(props.id);
+    const uploaderInfo = await clerkClient.users.getUser(image.userId);
+
+
+    return (
+        <div className="flex h-full">
+            <div className="w-2/3 flex items-center justify-center border-r ">
+                <img src={image.url} className="max-h-full max-w-full object-contain" alt={image.name} />
+            </div>
+            <div className="w-1/3 flex flex-col text-gray-200">
+                <div className="text-lg border-b text-center p-2">
+                    <span className="text-wrap">{image.name}</span>
+                </div>
+                <div className="flex flex-wrap p-2">
+                    <span className="p-1">Uploaded By: </span>
+                    <span className="p-1">{uploaderInfo.fullName}</span>
+                </div>
+                <div className="flex flex-wrap p-2">
+                    <span className="p-1">Created On: </span>
+                    <span className="p-1">{new Date(image.createdAt).toLocaleDateString()}</span>
+                </div>
+                <div className="flex flex-col items-center justify-center p-2 mt-auto border-t">
+                    <form
+                        action={async () => {
+                            "use server";
+                            await deleteImage(idAsNumber);
+                        }}
+                    >
+                        <Button type="submit" variant="destructive">
+                            Delete
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
